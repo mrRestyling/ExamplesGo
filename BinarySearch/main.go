b7da39af699a8c5f153a109bf2cc3d765e68b772
Egor Onekeev <sergeev_e_k@mail.ru>
2024-10-29 15:20:19 +0300

    ref

--- BinarySearch/main.go
+++ BinarySearch/main.go
@@ -10,9 +10,8 @@ func main() {
 	var arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 	// fmt.Println(LinearSort(arr, 100))
-	// fmt.Println(BinarySearch(arr, 100))
+	fmt.Println(BinarySearch(arr, 100))
 	// fmt.Println(JumpSearch(arr, 10))
-	fmt.Println(new(arr, 3))
 
 }
 
@@ -71,27 +70,5 @@ func JumpSearch(arr []int, s int) int {
 		}
 	}
 
-	return -1
-}
-
-func new(arr []int, s int) int {
-
-	low := 0
-	high := len(arr) - 1
-
-	// mid := len(arr) / 2
-
-	for low <= high {
-
-		mid := (low + high) / 2
-
-		if arr[mid] == s {
-			return mid
-		} else if arr[mid] < s {
-			low = mid + 1
-		} else {
-			high = mid - 1
-		}
-	}
 	return -1
 }
